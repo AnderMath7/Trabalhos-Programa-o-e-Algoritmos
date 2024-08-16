776135e3fdc586ae6e42fac29eecd8ba362a1aad
AnderMath7 <matheusanderson7@gmail.com>
2024-08-16 10:48:35 -0300

    criação da matéria e primeiro arquivo

---
+++ 3_semestre/Programacao_web/hello_world.js
@@ -0,0 +1,43 @@
+console.log('Hello, world!');
+/*
+const i = 1;
+
+if(i == 1){
+    const n = 3;
+}
+
+//console.log(n);
+
+var j = 3;
+j = 2;
+var j;
+//console.log(j);
+
+let z = 3;
+z = 2;
+//console.log(z);
+
+minhaStringA = 'texto 1';
+minhaStringB = 'texto "2"' + z;
+minhaStringC = `texto ${z}`;
+
+//console.log(minhaStringA);
+//console.log(minhaStringB);
+//console.log(minhaStringC);
+
+let n1 = .5;
+let n2 = 3;
+let n3 = 1.0;
+let n4 = "1.0" //String
+
+//console.log(n1);
+//console.log(n2);
+//console.log(n3);
+//console.log(n4);
+
+let c1;
+console.log(c1); //undefined - Sem atribuição
+
+let conexao = conectar(); //NULL - Com atribuição ou tentativa de atribuição, valor nulo
+console.log(conexao);
+*/
