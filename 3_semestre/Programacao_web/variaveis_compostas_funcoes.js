75b2604e4b7a9df66af147324d0dcad126de0051
AnderMath7 <matheusanderson7@gmail.com>
2024-08-23 10:21:18 -0300

    adição do conteúdo da aula 2

---
+++ 3_semestre/Programacao_web/variaveis_compostas_funcoes.js
@@ -0,0 +1,73 @@
+// Declara
+let notas = [9, 8.5, 7, "10", false];
+
+/* FUNCIONA | false ---> true
+const notas = [9, 8.5, 7, "10", false] 
+notas [4] = true;
+*/
+
+console.log(notas);
+
+//Verifica a quantidade de elemento da Array
+console.log(notas.length);
+
+//laço de repetição para percorrer o array
+for(let i = 0; i < notas.length; i++){
+    if(typeof notas[i] == 'number'){
+        if(notas[i] >= 7){
+            console.log("Aprovado!");
+        }else{
+            console.log("Reprovado!");
+        }
+    }else{
+        console.log("Nota inválida!");
+    }
+}
+
+/*
+Declaração de matriz 3x
+    2 1 0
+    0 1 0
+    1 2 1
+*/
+let A = [
+    [2, 1, 0],
+    [0, 1, 0],
+    [1, 2, 1]
+];
+console.log(A[2][1]);
+
+//objeto
+let capitais = {
+    //propriedades
+    DF: 'Brasília',
+    DF_DDD: 61,
+    BA: 'Salvador',
+    BA_DDD: 71,
+    RJ: 'Rio de Janeiro',
+    RJ_DDD: 21
+};
+
+console.log(capitais.DF);
+
+for (const key in capitais) {
+    const valor = capitais[key];
+    console.log(key + ' - ' + valor);
+}
+
+/**
+Função
+verifica se um número é par
+@param {integer} n
+@returns {boolean}
+*/
+function verificarNumeroPar(n){
+    if(n % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(verificarNumeroPar(2));
+console.log(verificarNumeroPar(3));
